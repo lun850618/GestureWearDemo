74dbeec532e96e99cca93674f583415df40b7757
李維倫 <lun850618@me.com>
2015-01-31 15:45:30 +0800

    Remove back slide.

---
+++ app/src/main/java/com/lun/tw/gesturestestwear/MainActivity.java
@@ -0,0 +1,48 @@
+package com.lun.tw.gesturestestwear;
+
+import android.app.Activity;
+import android.gesture.Gesture;
+import android.gesture.GestureLibraries;
+import android.gesture.GestureLibrary;
+import android.gesture.GestureOverlayView;
+import android.gesture.Prediction;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends Activity implements GestureOverlayView.OnGesturePerformedListener {
+    private GestureLibrary gestureLib;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        GestureOverlayView gestureOverlayView = new GestureOverlayView(this);
+        View inflate = getLayoutInflater().inflate(R.layout.activity_main, null);
+        gestureOverlayView.addView(inflate);
+        gestureOverlayView.addOnGesturePerformedListener(this);
+        gestureLib = GestureLibraries.fromRawResource(this, R.raw.gestures);
+        if (!gestureLib.load()) {
+            finish();
+        }
+        setContentView(gestureOverlayView);
+    }
+
+    @Override
+    public void onGesturePerformed(GestureOverlayView overlay, Gesture gesture) {
+        ArrayList<Prediction> predictions = gestureLib.recognize(gesture);
+        for (Prediction prediction : predictions) {
+            if (prediction.score > 4.0) {
+                Toast.makeText(this, prediction.name, Toast.LENGTH_SHORT)
+                        .show();
+                Toast.makeText(this, prediction.score+"", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
+    }
+
+}
